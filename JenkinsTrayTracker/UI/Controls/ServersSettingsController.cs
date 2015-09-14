ac37e9f01576192355ea9b2e681634c6a13ebc56
Andrew Everson <jeff2001@gmail.com>
2015-09-14 22:16:43 +0200

    Begin rename to Jenkins from Hudson

---
+++ JenkinsTrayTracker/UI/Controls/ServersSettingsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jenkins.TrayTracker.BusinessComponents;
+using Spring.Context.Support;
+using Jenkins.TrayTracker.Entities;
+using DevExpress.XtraBars;
+
+namespace Jenkins.TrayTracker.UI.Controls
+{
+    public class ServersSettingsController
+    {
+        ConfigurationService configurationService;
+        JenkinsService jenkinsService;
+
+        ServerListControl serverListControl;
+        ProjectListControl projectListControl;
+        BarStaticItem statusTextItem;
+        BarEditItem statusProgressItem;
+
+        public ServersSettingsController(ServerListControl serverListControl,
+            ProjectListControl projectListControl,
+            BarStaticItem statusTextItem, 
+            BarEditItem statusProgressItem)
+        {
+            this.serverListControl = serverListControl;
+            this.projectListControl = projectListControl;
+            this.statusTextItem = statusTextItem;
+            this.statusProgressItem = statusProgressItem;
+
+            configurationService = (ConfigurationService)ContextRegistry.GetContext().GetObject("ConfigurationService");
+            jenkinsService = (JenkinsService)ContextRegistry.GetContext().GetObject("JenkinsService");
+            serverListControl.ConfigurationService = configurationService;
+            projectListControl.ConfigurationService = configurationService;
+            projectListControl.JenkinsService = jenkinsService;
+        }
+
+        public void Initialize()
+        {
+            serverListControl.Initialize();
+            projectListControl.Initialize();
+        }
+
+        public void UpdateProjectList(Server server)
+        {
+            projectListControl.UpdateProjectList(server);
+        }
+
+        internal void SetStatus(string status, bool updating)
+        {
+            statusTextItem.Caption = status;
+            statusProgressItem.Visibility = updating ? BarItemVisibility.Always : BarItemVisibility.Never;
+        }
+    }
+}
---
+++ JenkinsTrayTracker/Utils/UIUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using Common.Logging;
+using DevExpress.XtraEditors;
+using System.Windows.Forms;
+
+namespace Jenkins.TrayTracker.Utils
+{
+    public static class UIUtils
+    {
+        public static void OpenWebPage(string url, ILog logger)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch (Exception ex)
+            {
+                logger.Warn("Failed opening page: " + url, ex);
+
+                string errorMessage = String.Format(JenkinsTrayTrackerResources.ErrorBoxMessage, ex.Message);
+                XtraMessageBox.Show(errorMessage, JenkinsTrayTrackerResources.ErrorBoxCaption,
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
